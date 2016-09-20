6f2766df53a00d758c9f3414c6804fb7f6332599
nguyennghi3489 <nguyennghi3489@gmail.com>
2016-09-20 10:52:58 +0700

    Add utilities file

---
+++ utilities.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+module.exports = {
+    sendMail:function(email, token){
+		var transporter = nodemailer.createTransport('smtps://nguyennghi3489@gmail.com:03041989@smtp.gmail.com');
+			console.log(email);
+			// setup e-mail data with unicode symbols
+			var mailOptions = {
+			    from: '<nguyennghi3489@gmail.com>', // sender address
+			    //to: email, // list of receivers
+			    to : 'nguyennghi3489@gmail.com',
+			    subject: 'Hello ✔', // Subject line
+			    text: 'Hello world 🐴', // plaintext body
+			    html: "<b>Hello world 🐴</b>"+ "<a href='http://localhost:3000/api/confirmMail?token="+ token + "' />Click Here to Active Your Account</a>" // html body
+			};
+
+			// send mail with defined transport object
+			transporter.sendMail(mailOptions, function(error, info){
+			    if(error){
+			        return console.log(error);
+			    }
+			    console.log('Message sent: ' + info.response);
+			});
+	}
+};
