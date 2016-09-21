52ecc4cc48bff4f725fbfeb0a092ac880959e53c
nguyennghi3489 <nguyennghi3489@gmail.com>
2016-09-21 17:23:35 +0700

    Update Mail url

--- routes/mail.js
+++ routes/mail.js
@@ -45,7 +45,7 @@ module.exports = function(app) {
 			    }
 			  }
 			  response.writeHead(302, {
-				  'Location': 'http://localhost:8000'
+				  'Location': 'http://52.43.102.187'
 				  //add other headers here...
 			  });
 	          response.end();
